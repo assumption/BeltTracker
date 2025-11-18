8b57f6057c4139d82b064684fd6274ff14b9410f
Alexander Chamberlain <alxchamberlain@gmail.com>
2025-11-18 02:20:56 -0800

    Make starting month in calendar dynamic based on initial screen size

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,9 +4,10 @@ export const dateLib = new DateLib();
 export const sundayIndex = 0;
 export const saturdayIndex = 6;
 
-export function lastMonth(): Date {
+export function getStartingMonth(numberOfMonthsInitiallyRendered: number): Date {
+    const offset = numberOfMonthsInitiallyRendered > 2 ? 1 : 0;
     const date = new Date();
-    date.setMonth(date.getMonth() - 1);
+    date.setMonth(date.getMonth() - offset);
     return date;
 }
 
